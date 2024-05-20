4d3024d9b94b3ea410a1c95768277124e0990422
Anri Khachaturov <anrkhach@c2r16s2.42yerevan.am>
2024-05-20 19:46:41 +0400

    a

---
+++ sources/minishell.c
@@ -0,0 +1,15 @@
+#include "minishell.h"
+#include "tokenization.h"
+#include "utils.h"
+
+int	main(void)
+{
+	//t_token	*token_list;
+	char	*cmd_line;
+
+	//token_list = NULL;
+	while (1)
+	{
+		cmd_line = readline("MINISHELL HOP:");
+	}
+}
