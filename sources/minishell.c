27a5e92bade2597f759e9bf2090bbd187efaac76
Vardan Barseghyan <vbarsegh@c2r16s3.42yerevan.am>
2024-06-28 21:11:22 +0400

    hnnn

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 int	main(int ac, char **av, char **env)
 {
 	t_token	*tokens;
-	// char	*cmd_line;
+	 char	*cmd_line;
 	t_shell	*shell;
 
 	ac = 0;
@@ -14,36 +14,36 @@ int	main(int ac, char **av, char **env)
 	tokens = NULL;
 	shell = malloc(sizeof(t_shell));
 	shell->envr = init_env(shell->envr, env);
-	while(shell->envr)
-	{
-		printf("key=%s\n",shell->envr->value);
-		shell->envr = shell->envr->next;
-	}
-	// shell->cmds = NULL;
-	// check_env(shell);
-	// while (1)
+	// while(shell->envr)
 	// {
-	// 	cmd_line = readline("VIBERSEIJSHELL:");
-	// 	add_history(cmd_line);
-	// 	if (cmd_line && *cmd_line)
-	// 	{
-	// 		tokenization(cmd_line, &tokens);
-	// 		dolarni2(&tokens, env);
-	// 		// chakertni(&tokens);
-	// 		token_to_cmds(shell, tokens);
-	// 		// run_cmds(shell->cmds, env);
-	// 		print_token_list(tokens);
-	// 		int i = 0;
-	// 		while (shell->cmds)
-	// 		{
-	// 			i = 0;
-	// 			while (shell->cmds->cmd_args[i])
-	// 				printf("%s\n", shell->cmds->cmd_args[i++]);
-	// 			printf("path -- %s\n", shell->cmds->cmd_path);
-	// 			shell->cmds = shell->cmds->next;
-	// 		}
-	// 	}
-	// 	ft_token_list_clear(&tokens);
-	// 	free(cmd_line);
+	// 	printf("key=%s\n",shell->envr->value);
+	// 	shell->envr = shell->envr->next;
 	// }
+	shell->cmds = NULL;
+	check_env(shell);
+	while (1)
+	{
+		cmd_line = readline("VIBERSEIJSHELL:");
+		add_history(cmd_line);
+		if (cmd_line && *cmd_line)
+		{
+			tokenization(cmd_line, &tokens);
+			dolarni2(&tokens, env);
+			// chakertni(&tokens);
+			token_to_cmds(shell, tokens);
+			// run_cmds(shell->cmds, env);
+			print_token_list(tokens);
+			int i = 0;
+			while (shell->cmds)
+			{
+				i = 0;
+				while (shell->cmds->cmd_args[i])
+					printf("%s\n", shell->cmds->cmd_args[i++]);
+				printf("path -- %s\n", shell->cmds->cmd_path);
+				shell->cmds = shell->cmds->next;
+			}
+		}
+		ft_token_list_clear(&tokens);
+		free(cmd_line);
+	}
 } 
