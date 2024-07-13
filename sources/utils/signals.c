fef9730ba26fe5b5569cbc39d1d7a0ea84fc9867
Vardan Barseghyan <vbarsegh@c2r16s3.42yerevan.am>
2024-07-14 01:03:08 +0400

    signalneri bacatrutyun yani

--- sources/utils/signals.c
+++ sources/utils/signals.c
@@ -20,19 +20,21 @@ void	disable_echoctl(void)
 {
 	struct termios	term;
 
-	if (tcgetattr(STDIN_FILENO, &term) != 0)
+	if (tcgetattr(STDIN_FILENO, &term) != 0)//tcgetattr считывает текущие параметры терминала, связанного с файловым дескриптором STDIN_FILENO (стандартный ввод), и сохраняет их в структуру term.
 		return ;
-	term.c_lflag &= ~(ECHOCTL);
-	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	term.c_lflag &= ~(ECHOCTL);//&= ~(ECHOCTL) գործողությունը մաքրում է ECHOCTL դրոշը, դրանով իսկ անջատելով այս կառավարման նիշերի ցուցադրումը:
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);//Функция tcsetattr устанавливает новые параметры терминала, которые находятся в структуре term.
+											// STDIN_FILENO указывает, что настройки применяются к стандартному вводу (обычно клавиатуре).
+											// TCSANOW указывает, что изменения должны быть применены немедленно.
 }
 
 void	ctrl_d(int sig)
 {
 	(void)sig;
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
-	rl_cleanup_after_signal();
+	rl_on_new_line();//nor readline-i toxa tali//сообщает библиотеке Readline, что текущая строка ввода завершена и нужно начать новую строку//MINIVIBERSEIJ:-i masina xosqy
+	rl_replace_line("", 0);// используется для замены текущей строки ввода на указанную строку, arajiny Указатель на строку, которая заменит текущую строку ввода.ete datarki texy grenq Anri apa ctrl c enluc kberi taza tox senc-MINIVIBERSEIJ: Anri,dnum enq datark vor Anrin chlni
+	rl_redisplay();//используется для перерисовки строки ввода esi vor chlni ctrl c aneluc en mer MINIVIBERSEIJ:-@ chi grvi minchev hraman chgrenq
+	rl_cleanup_after_signal();// используется для очистки внутреннего состояния библиотеки Readline после обработки сигнала. Это помогает предотвратить некорректное поведение и утечки памяти, которые могут возникнуть, если сигнал прерывает ввод пользователя.
 }
 
 void	ctrl_bckslash(void)
